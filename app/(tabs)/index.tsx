68917ed916bf3f79b41031b1612978bb0616b1aa
shaker <hydrashaker@gmail.com>
2025-05-21 23:06:33 +0530

    tailwind setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,15 +2,8 @@ import { Text, View} from "react-native"
 
 export default function Index(){
   return(
-    <View
-      
-      style={{
-        flex:1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-      >
-        <Text>Home</Text>
+    <View className="flex-1 justify-center items-center">
+        <Text className="text-5xl text-blue-500 font-bold" >Welcome !</Text>
       </View>
   )
-}
+}                    
