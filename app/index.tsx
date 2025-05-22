07acd3278db88abf1d078367f67884783729863f
shaker <hydrashaker@gmail.com>
2025-05-22 11:56:22 +0530

    completed all set ups

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,8 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-blue-500 font-bold">Welcome !</Text>
+      <Text className="text-3xl text-yellow-400 font-bold">This is a movie app</Text>
+    
     </View>
   );
 }
